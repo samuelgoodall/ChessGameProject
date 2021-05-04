5346b8411131e309f2131fd31eec7912ffa20367
Samuel Goodall <samuel.goodall@exxcellent.de>
2021-05-04 21:43:53 +0200

    created GamePiece and Peasant

---
+++ Game/src/main/java/GamePieces/Peasant.java
@@ -0,0 +1,36 @@
+package GamePieces;
+
+import com.github.javaparser.utils.Pair;
+
+import java.util.ArrayList;
+
+public class Peasant extends GamePiece{
+
+    public Peasant(int posX, int posY,String colour) {
+        super(posX, posY,colour);
+    }
+
+    @Override
+    public ArrayList<Pair<Integer, Integer>> getPossibleMoves() {
+
+        ArrayList<Pair<Integer, Integer>> movearray = new ArrayList<>();
+        int sign = 1;
+        int moved=0;
+        if(colour.equals("black")){
+            sign = -1;
+        }
+        if(!wasMoved){
+           moved = 1;
+        }
+        int x=this.position.a;
+        int y=this.position.b;
+
+        //move peasant normally
+        movearray.add(new Pair<>(x, y + (sign * (moved +1))));
+        movearray.add(new Pair<>(x+ sign,y+ sign));
+
+        return movearray;
+    }
+
+
+}
